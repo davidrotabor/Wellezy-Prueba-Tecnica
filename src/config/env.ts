be88e4397594dd6439c7209cd57ca7c97bdd5e92
davidrotaborudea <davids.rodriguez@udea.edu.co>
2024-11-14 12:52:46 -0500

    FTR deploy vercel

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,4 @@
 export const env = {
     AIRLINES_BASE_URL: 'https://staging.travelflight.aiop.com.co/api',
-    BACK_BASE_URL: 'http://localhost:8000/api'
+    BACK_BASE_URL: 'https://wellezy-prueba-back-production.up.railway.app/api'
 }
